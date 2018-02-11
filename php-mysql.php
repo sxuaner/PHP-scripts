b15e30000d2ae3bda866a956513132884ddada98
Xuan Song <songxuan319@gmail.com>
2018-02-11 20:16:03 +0100

    Modified if-else block in try-catch. still not succeed in finding out how to print the result of a select query.

--- php-mysql.php
+++ php-mysql.php
@@ -3,6 +3,10 @@
 /*  This program simply connects to mysql and fecth data from user@test (tablen@dabase)
     With mysql_connect() deprecated. Use PDO instead.
     However, error infomation during debugging was not show in STDOUT. Find out why
+    The failuer was caused by wrong query "select * from sx". sx was the wrong table name instead of user.
+    
+    How to print out database query result??
+
 */
 
 // db creadentials
@@ -12,7 +16,7 @@ $dbpass = "songxuan";
 $dbname = "test";
 $table = "user";
 
-$query = "SELECT * FROM user";
+$query = "SELECT * FROM sx";
 try{
 
     $dblink = new PDO('mysql:host=localhost;dbname=test',$dbuser,$dbpass);
@@ -22,16 +26,21 @@ try{
             print_r($row["id"] . "\n");
             print_r($row["name"]. "\n");
         }
-    }
+    } 
     else{
-        echo "Connection failed\n";
-    }
+        // Please note that at this point, the connection has been successfully made. If PDO_statements is FALSE, it means it has failed to fetch data
+        // PDO::query() returns a PDOStatement object, or FALSE on failure. 
+        echo "Failed to fetch data\n";
+    } 
     // close a db conn
     $PDO_statements = null;
     $dblink = null;
 
 }catch(PDOException $e){
+    print "Error!!";
     print $e->getMessage();
+    print $e->getTrace();
+    print $e->getCode();
     die();
 }
 ?>
